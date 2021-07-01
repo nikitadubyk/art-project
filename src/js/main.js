5998569d9c029476975120f7233a29fbcb154f82
Nikita <nikita_dubyk@mail.ru>
2021-07-01 10:28:40 +0300

    add simple work cards with db.json

--- src/js/main.js
+++ src/js/main.js
@@ -15,5 +15,5 @@ window.addEventListener('DOMContentLoaded', () => {
     mask('[name="phone"]');
     checkTextInputs('[name="message"]');
     checkTextInputs('[name="name"]');
-    showMoreStyles('.button-styles', '.styles-2');
+    showMoreStyles('.button-styles', '#styles .row');
 });
